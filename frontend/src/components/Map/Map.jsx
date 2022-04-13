ad0151bd6c4f1c913469aa4a1b218a332fdcbd3c
Romain MURA <romain@mura.io>
2022-04-14 00:08:49 +0200

    start pages/BigMap

--- frontend/src/components/Map/Map.jsx
+++ frontend/src/components/Map/Map.jsx
@@ -1,16 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "./Map.scss";
 
-function Map({ getPos }) {
-  const [pos, setPos] = useState(getPos());
+function Map() {
+  // const [pos, setPos] = useState(getPos());
+
   return (
-    <MapContainer center={pos} zoom={13} id="map">
+    <MapContainer center={[]} zoom={13} id="map">
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={pos}>
+      <Marker position={[]}>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
