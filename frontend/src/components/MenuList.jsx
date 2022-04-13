11383cc8f1286d5c8e006b5e0949a489009b501c
JimmyVogtGit <vogt.jimmy18@gmail.com>
2022-04-13 15:22:54 +0200

    modify div 1

--- frontend/src/components/MenuList.jsx
+++ frontend/src/components/MenuList.jsx
@@ -4,7 +4,7 @@ import "./menuList.css";
 export default function MenuList() {
   return (
     <div className="menu-list">
-      <div>composant 1</div>
+      <div>composant toto</div>
       <div>composant 2</div>
       <div>composant 3</div>
     </div>
