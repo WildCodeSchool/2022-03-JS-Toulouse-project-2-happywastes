579e7584f27ce234373f53c351ce4965fa15975f
Lucas-jimenez-dev <pro.lucasjimenez@gmail.com#Downpower2302!Lucas-jimenez-devgit config --global user.email pro.lucasjimenez@gmail.comgit config --global user.email pro.lucasjimenez@gmail.com#>
2022-04-14 12:05:47 +0200

    add BackButton component on pages/dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,9 +1,11 @@
+import BackButton from "../components/BackButton/BackButton";
 import NavBottom from "../components/Common/NavBottom/NavBottom";
 import ProfileButton from "../components/Common/ProfileButton/ProfileButton";
 
 export default function Dashboard() {
   return (
     <div className="dashboard">
+      <BackButton />
       <ProfileButton />
       <NavBottom />
     </div>
