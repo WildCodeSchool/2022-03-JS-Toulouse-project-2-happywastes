bf0cf69734f9b69b1e1a1987c53e3d6fb13369a6
JoeriKaiser <joeri.kaiser@hotmail.com>
2022-04-14 11:57:08 +0200

    Changed top button for linking

--- frontend/src/components/BackButton/BackButton.jsx
+++ frontend/src/components/BackButton/BackButton.jsx
@@ -1,11 +1,6 @@
-import { Link } from "react-router-dom";
 import React from "react";
 import "./back-button.css";
 
 export default function BackButton() {
-  return (
-    <Link to="settings">
-      <div className="back-button">coucou</div>
-    </Link>
-  );
+  return <div className="back-button">coucou</div>;
 }
