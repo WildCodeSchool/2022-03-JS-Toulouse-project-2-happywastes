8e182a792c61532ee72e0b687e84e323a63b6f57
JimmyVogtGit <vogt.jimmy18@gmail.com>
2022-04-14 16:38:25 +0200

    create back button fonctional with url-1

--- frontend/src/components/BackButton/BackButton.jsx
+++ frontend/src/components/BackButton/BackButton.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import "./back-button.css";
-import image from "../../assets/img/back-button.png";
+import { useNavigate } from "react-router-dom";
 
 export default function BackButton() {
+  const navigate = useNavigate();
+
+  function goBack() {
+    navigate(-1);
+  }
+
   return (
     <div className="back-button">
-      <img src={image} alt="back-button" className="back-btn-img" />
+      <button type="button" onClick={goBack} className="btn-back">
+        &nbsp;
+      </button>
     </div>
   );
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,9 @@
 import MenuList from "../components/MenuList";
 import ProfileButton from "../components/Common/ProfileButton/ProfileButton";
-import BackButton from "../components/BackButton/BackButton";
 
 export default function Home() {
   return (
     <div className="home">
-      <BackButton />
       <ProfileButton />
       <MenuList />
     </div>
