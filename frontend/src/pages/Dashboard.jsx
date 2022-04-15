5bd167dcf3237da891e3f7053bf994b0812e015d
Lucas-jimenez-dev <pro.lucasjimenez@gmail.com#Downpower2302!Lucas-jimenez-devgit config --global user.email pro.lucasjimenez@gmail.comgit config --global user.email pro.lucasjimenez@gmail.com#>
2022-04-15 11:34:45 +0200

    add icon dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,22 +1,27 @@
 import BackButton from "../components/BackButton/BackButton";
 import NavBottom from "../components/Common/NavBottom/NavBottom";
 import ProfileButton from "../components/Common/ProfileButton/ProfileButton";
+import "./Dashboard.css";
 
 export default function Dashboard() {
-  //   const reward = [
-  //     {
-  //       id: 1,
-  //       badge: "plastic bottle",
-  //       icon: "src/assets/img/iconYellow-smaill.png",
-  //     },
-  // ];
+  const reward = [
+    {
+      id: 1,
+      badge: "plastic bottle",
+      icon: "src/assets/img/iconYellow-small.png",
+    },
+  ];
 
   return (
     <div className="dashboard">
-      <ul>
-        <li />
-        <li />
-      </ul>
+      <div className="Container-Avatar" />
+      {reward.map((element) => (
+        <ul className="Container-Reward">
+          <li>
+            <img src={element.icon} alt="" />
+          </li>
+        </ul>
+      ))}
       <BackButton />
       <ProfileButton />
       <NavBottom />
