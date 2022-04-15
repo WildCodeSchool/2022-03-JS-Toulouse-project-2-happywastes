1e988e98d4907baadb4c41a0dd7880ae4bf5ac81
JoeriKaiser <joeri.kaiser@hotmail.com>
2022-04-15 10:09:27 +0200

    Finished avatar temporary placeholder

--- frontend/src/components/Common/Avatar/Avatar.jsx
+++ frontend/src/components/Common/Avatar/Avatar.jsx
@@ -4,7 +4,14 @@ import React from "react";
 function Avatar() {
   return (
     <div className="avatar-container">
-      <div className="hexagon" />
+      <div className="hexagon-border-2">
+        <div className="hexagon-border-1">
+          <div className="hexagon">
+            <img src="../src/assets/img/Phill-Good.png" alt="cat" srcSet="" />
+          </div>
+        </div>
+      </div>
+      <h2 className="avatar-title">PHILL GOOD</h2>
     </div>
   );
 }
