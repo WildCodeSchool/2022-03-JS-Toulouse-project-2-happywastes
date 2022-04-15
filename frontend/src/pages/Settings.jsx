1203b0a9cb261a5c153a8bd8062239b9d8d37641
JimmyVogtGit <vogt.jimmy18@gmail.com>
2022-04-15 10:35:30 +0200

    resolve conflits

--- frontend/src/pages/Settings.jsx
+++ frontend/src/pages/Settings.jsx
@@ -1,8 +1,6 @@
 import React from "react";
-
 import Avatar from "../components/Common/Avatar/Avatar";
 import NavBottom from "../components/Common/NavBottom/NavBottom";
-
 import BackButton from "../components/BackButton/BackButton";
 
 export default function Settings() {
