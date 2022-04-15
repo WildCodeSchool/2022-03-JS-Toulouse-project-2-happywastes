a382a5683c04bf85ce7fff44544fdccc8b11fc82
Lucas-jimenez-dev <pro.lucasjimenez@gmail.com#Downpower2302!Lucas-jimenez-devgit config --global user.email pro.lucasjimenez@gmail.comgit config --global user.email pro.lucasjimenez@gmail.com#>
2022-04-15 08:20:00 +0200

    create page dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,13 +1,25 @@
-import BackButton from "../components/BackButton/BackButton";
-import NavBottom from "../components/Common/NavBottom/NavBottom";
-import ProfileButton from "../components/Common/ProfileButton/ProfileButton";
+// import BackButton from "../components/BackButton/BackButton";
+// import NavBottom from "../components/Common/NavBottom/NavBottom";
+// import ProfileButton from "../components/Common/ProfileButton/ProfileButton";
 
-export default function Dashboard() {
-  return (
-    <div className="dashboard">
-      <BackButton />
-      <ProfileButton />
-      <NavBottom />
-    </div>
-  );
-}
+// export default function Dashboard() {
+//   const reward = [
+//     {
+//       id: 1,
+//       badge: "plastic bottle",
+//       icon: "src/assets/img/iconYellow-smaill.png",
+//     },
+// ];
+
+//   return (
+//     <div className="dashboard">
+//       <ul>
+//         <li />
+//         <li />
+//       </ul>
+//       <BackButton />
+//       <ProfileButton />
+//       <NavBottom />
+//     </div>
+//   );
+// }
