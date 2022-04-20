b985ddf35af33105caee66d06d59dc7049b1482b
Romain MURA <romain@mura.io>
2022-04-20 15:29:52 +0200

    refactoring map component

--- frontend/src/components/Map/Popup/Popup.jsx
+++ frontend/src/components/Map/Popup/Popup.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import "./Popup.scss";
-
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faClose } from "@fortawesome/free-solid-svg-icons";
+import rewards from "../../Dashboard/DataReward";
 
 library.add(faClose);
 
@@ -19,6 +19,13 @@ function Popup({ setShowPopup, title, text }) {
         </div>
         <div className="card-body">
           <p>{text}</p>
+          <ul>
+            {rewards.slice(rewards.length - 4).map((reward) => (
+              <li>
+                <img src={reward.img} alt="" className="reward-small" />
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </div>
