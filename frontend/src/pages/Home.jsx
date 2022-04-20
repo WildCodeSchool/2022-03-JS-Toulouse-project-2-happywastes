9eedb5dcab88e721462200514990835c63962900
JimmyVogtGit <vogt.jimmy18@gmail.com>
2022-04-20 10:41:44 +0200

    page level incorpored on the Home page for testing

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,13 @@
-import MenuList from "../components/MenuList";
-import ProfileButton from "../components/Common/ProfileButton/ProfileButton";
+// import MenuList from "../components/MenuList";
+// import ProfileButton from "../components/Common/ProfileButton/ProfileButton";
+import Level from "./Level";
 
 export default function Home() {
   return (
     <div className="home">
-      <MenuList />
-      <ProfileButton />
+      <Level />
+      {/* <MenuList />
+      <ProfileButton /> */}
     </div>
   );
 }
--- frontend/src/pages/Level.jsx
+++ frontend/src/pages/Level.jsx
@@ -1,5 +1,12 @@
 import React from "react";
+import BackButton from "../components/BackButton/BackButton";
+import ProfileButton from "../components/Common/ProfileButton/ProfileButton";
 
 export default function Level() {
-  return <div>coucou page de level</div>;
+  return (
+    <div>
+      <BackButton />
+      <ProfileButton />
+    </div>
+  );
 }
