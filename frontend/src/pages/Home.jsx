4a514acc9fb73d587d4f7bd5535cb07b5ec949f7
Lucas-jimenez-dev <pro.lucasjimenez@gmail.com#Downpower2302!Lucas-jimenez-devgit config --global user.email pro.lucasjimenez@gmail.comgit config --global user.email pro.lucasjimenez@gmail.com#>
2022-04-21 12:07:22 +0200

    deleted files splashscreen

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,9 @@
-import SplashScreen from "../components/SplashScreen/SplashScreen";
 import MenuList from "../components/MainMenu/MainMenu";
 import ProfileButton from "../components/ProfileButton/ProfileButton";
 
 export default function Home() {
   return (
     <div className="home">
-      <SplashScreen />
       <MenuList />
       <ProfileButton />
     </div>
