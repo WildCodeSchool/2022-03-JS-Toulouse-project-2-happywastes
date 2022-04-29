f15dd72f73655ab075b14607bfc03d9edc3a4a10
Romain MURA <romain@mura.io>
2022-04-29 11:20:28 +0200

    Update Home.jsx
    Try to lint.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,29 +1,29 @@
-import { motion, AnimatePresence } from "framer-motion";
-import { useState } from "react";
-import MainMenu from "../components/MainMenu/MainMenu";
-import Login from "./Login";
-import ProfileButton from "../components/ProfileButton/ProfileButton";
-import variants from "../assets/js/variants";
+import { motion, AnimatePresence } from 'framer-motion';
+import { useState } from 'react';
+import MainMenu from '../components/MainMenu/MainMenu';
+import Login from './Login';
+import ProfileButton from '../components/ProfileButton/ProfileButton';
+import variants from '../assets/js/variants';
 
 export default function Home() {
   const [showMainMenu, setShowMainMenu] = useState(true);
   return (
-    <div className="home">
+    <div className='home'>
       {showMainMenu ? (
         <Login setShowMainMenu={setShowMainMenu} />
       ) : (
-          <AnimatePresence exitBeforeEnter>
-            <motion.div
-              variants={variants}
-              key="Home"
-              initial="hidden"
-              animate="visible"
-              exit="exit"
-            >
-              <MenuList />
-              <ProfileButton />
-            </motion.div>
-          </AnimatePresence>
+        <AnimatePresence exitBeforeEnter>
+          <motion.div
+            variants={variants}
+            key='Home'
+            initial='hidden'
+            animate='visible'
+            exit='exit'
+          >
+            <MainMenu />
+            <ProfileButton />
+          </motion.div>
+        </AnimatePresence>
       )}
     </div>
   );
