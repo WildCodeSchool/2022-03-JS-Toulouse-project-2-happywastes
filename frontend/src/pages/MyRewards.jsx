498e68c0d9174d8f21aad123d4bd0ba920782a5d
JimmyVogtGit <vogt.jimmy18@gmail.com>
2022-05-04 11:59:06 +0200

    Fix Switch on component MyRewardsSwitch

--- frontend/src/pages/MyRewards.jsx
+++ frontend/src/pages/MyRewards.jsx
@@ -12,14 +12,27 @@ import variants from "../assets/js/variants";
 
 export default function MyRewards() {
   const [slicing, setSlicing] = useState(0);
-  const pagination = Math.ceil(rewards.length / 5);
   const lengthRewardsData = rewards.length;
+  const pagination = Math.ceil(lengthRewardsData / 5);
+  const [count, setCount] = useState(1);
 
   const nextSlicing = () => {
-    setSlicing(slicing + 5);
+    if (count >= pagination) {
+      setSlicing(0);
+      setCount(1);
+    } else {
+      setSlicing(slicing + 5);
+      setCount(count + 1);
+    }
   };
   const previousSlicing = () => {
-    setSlicing(slicing - 5);
+    if (count <= 1) {
+      setSlicing((pagination - 1) * 5);
+      setCount(pagination);
+    } else {
+      setSlicing(slicing - 5);
+      setCount(count - 1);
+    }
   };
 
   return (
@@ -39,6 +52,7 @@ export default function MyRewards() {
           <MyRewardsList slicing={slicing} />
           <MyRewardsSwitch
             slicing={slicing}
+            setSlicing={setSlicing}
             pagination={pagination}
             funcNext={nextSlicing}
             funcPrevious={previousSlicing}
