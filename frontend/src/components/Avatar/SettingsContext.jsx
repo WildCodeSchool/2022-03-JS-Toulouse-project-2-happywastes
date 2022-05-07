6734288386e7e3cfd2fe9fbdf83db204e501a9c0
JoeriKaiser <joeri.kaiser@hotmail.com>
2022-05-07 16:59:59 +0200

    Fixed missing opening bracket in SettingsContext

---
+++ frontend/src/components/Avatar/SettingsContext.jsx
@@ -0,0 +1,18 @@
+import { createContext, useState, useMemo } from "react";
+
+export const SettingsContext = createContext();
+
+function SettingsProvider({ children }) {
+  const [userSettings, setUserSettings] = useState("PHILLY GOODY");
+  const userName = useMemo(
+    () => ({ userSettings, setUserSettings }),
+    [userSettings]
+  );
+  return (
+    <SettingsContext.Provider value={userName}>
+      {{ children }}
+    </SettingsContext.Provider>
+  );
+}
+
+export default SettingsProvider;
